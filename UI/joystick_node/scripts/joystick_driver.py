84b70fc7b34dc4a9ef4ea29b1dd23793b1fd03db
shawbicc <faiyazsabik99@gmail.com>
2024-03-10 13:39:47 +0600

    added speed control feature

--- UI/joystick_node/scripts/joystick_driver.py
+++ UI/joystick_node/scripts/joystick_driver.py
@@ -2,7 +2,7 @@
 
 import rospy
 from sensor_msgs.msg import Joy
-from std_msgs.msg import Twist
+from geometry_msgs.msg import Twist
 
 class JoyToTwist:
     def __init__(self):
@@ -15,13 +15,40 @@ class JoyToTwist:
         # Create a subscriber for the /joy topic
         rospy.Subscriber('/joy', Joy, self.joystick_callback)
 
+	# Controller variables
+	self.SCALE = 0.5
+	self.SCALING_FACTOR = 0.05
+
     def joystick_callback(self, data):
         # Instantiate a Twist message
         twist = Twist()
 
+	# increase SCALE if UP BUTTON is pressed
+	if data.axes[5] > 0.9 and self.SCALE < 1.0:
+		self.SCALE = self.SCALE + self.SCALING_FACTOR
+
+	# reduce SCALE if DOWN BUTTON is pressed
+	if data.axes[5] < -0.9 and self.SCALE > 0.0:
+		self.SCALE = self.SCALE - self.SCALING_FACTOR
+
+	if self.SCALE > 1.0:
+		self.SCALE = 1.0
+	if self.SCALE < -1.0:
+		self.SCALE = -1.0
+
         # These indices might need adjustment depending on your joystick
-        twist.linear.x = (data.buttons[7] - data.buttons[6]) * 0.5  # Scale as necessary
-        twist.angular.z = data.axes[0] * (-0.5)  # Scale as necessary
+
+        twist.linear.x = (data.buttons[7] - data.buttons[6]) * self.SCALE  # Scale as necessary
+	if twist.linear.x > 1.0:
+		twist.linear.x = 1.0
+	if twist.linear.x < -1.0:
+		twist.linear.x = -1.0
+        
+	twist.angular.z = data.axes[0] * (-self.SCALE)  # Scale as necessary
+	if twist.angular.z > 1.0:
+		twist.angular.z = 1.0
+	if twist.angular.z < -1.0:
+		twist.angular.z = -1.0
 
         # Publish the Twist message to the /cmd_vel topic
         self.velocity_publisher.publish(twist)
